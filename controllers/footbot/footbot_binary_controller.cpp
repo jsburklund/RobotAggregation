89d944f53ff96e52565e7a1224f7aa10e0a106e7
jsburklund <robomower9508@gmail.com>
2018-04-15 14:44:18 -0400

    Parse 16-bit IDs

--- controllers/footbot/footbot_binary_controller.cpp
+++ controllers/footbot/footbot_binary_controller.cpp
@@ -1,22 +1,47 @@
 #include <argos3/core/utility/logging/argos_log.h>
 #include "footbot_binary_controller.h"
 
+#include <cstdio>
+
 static CRange<Real> WHEEL_ACTUATION_RANGE(-5.0f, 5.0f);
 
-CFootBotBinaryController::CFootBotBinaryController() : m_pcWheels(nullptr), m_params{0, 0, 0, 0} {
+CFootBotBinaryController::CFootBotBinaryController() : m_pcWheels(nullptr), m_params{0, 0, 0, 0} {//}, m_pcRABAct(NULL), m_pcRABSens(NULL) {
 }
 
 void CFootBotBinaryController::Init(TConfigurationNode &t_node) {
   try {
     m_pcWheels = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+    m_pcRABAct = GetActuator<CCI_RangeAndBearingActuator  >("range_and_bearing" );
+    m_pcRABSens = GetSensor <CCI_RangeAndBearingSensor >("range_and_bearing" );
   }
   catch (CARGoSException &ex) {
     THROW_ARGOSEXCEPTION_NESTED("Error initializing sensors/actuators", ex);
   }
+
+  // Parse the ID number
+  std::string my_idstr = GetId();
+  std::sscanf(my_idstr.c_str(), "fb%d", &my_id);
+
+
+  // Finish setup
+  Reset();
+  Reset();
+}
+
+void CFootBotBinaryController::Reset() {
+  argos::LOG <<"Called Reset\n";
+  m_pcRABAct->Reset();
+  my_id = my_id+1000;
+  m_pcRABAct->SetData(0, (uint8_t) my_id);
+  m_pcRABAct->SetData(1, (uint8_t) (((uint16_t) my_id)>>8));
+  my_id = my_id-1000;
 }
 
 void CFootBotBinaryController::ControlStep() {
   unsigned int I; // TODO: figure this out from other robots or mock sensor
+
+  GetKinSensorVal();
+
   m_pcWheels->SetLinearVelocity(-7, -10);
   return;
   switch (I) {
@@ -45,6 +70,27 @@ void CFootBotBinaryController::SetParameters(const size_t num_params, const Real
   for (size_t i = 0; i < num_params; ++i) {
     m_params[i] = params[i];
   }
+}
+
+unsigned int CFootBotBinaryController::GetKinSensorVal() {
+  // Get the other robots range and bearing
+  const CCI_RangeAndBearingSensor::TReadings& tMsgs = m_pcRABSens->GetReadings();
+  // Look at all robots that have sent a message
+  if(! tMsgs.empty()) {
+    if (my_id == 0) {
+      argos::LOG<<"Num neighbors: "<<tMsgs.size()<<"\n";
+    }
+    for (size_t i=0; i<tMsgs.size(); i++) {
+      Real range  = tMsgs[i].Range;
+      uint16_t rid =  ((uint16_t) tMsgs[i].Data[1])<<8 | ((uint16_t) tMsgs[i].Data[0]);
+      Real bearing = tMsgs[i].HorizontalBearing.GetValue();
+      if (my_id == 0) {
+        argos::LOG << "RID ["<<rid<<"]: dist "<<range<<"ang "<<bearing<<"\n";
+      }
+    }
+  }
+
+  return 0;
 }
 
 REGISTER_CONTROLLER(CFootBotBinaryController, "footbot_binary_controller")
--- controllers/footbot/footbot_binary_controller.h
+++ controllers/footbot/footbot_binary_controller.h
@@ -5,6 +5,8 @@
 #include <argos3/core/control_interface/ci_controller.h>
 #include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
 #include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_proximity_sensor.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_actuator.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_range_and_bearing_sensor.h>
 
 using namespace argos;
 
@@ -18,14 +20,23 @@ class CFootBotBinaryController : public CCI_Controller {
 
   void Init(TConfigurationNode &t_node) override;
 
+  void Reset();
+
   void ControlStep() override;
 
   void SetParameters(size_t num_params, const Real *params);
+
+  unsigned int GetKinSensorVal();
 
  private:
 
   CCI_DifferentialSteeringActuator *m_pcWheels;
   std::array<Real, GENOME_SIZE> m_params;
-
+  /* Pointer to the range-and-bearing actuator */
+  CCI_RangeAndBearingActuator* m_pcRABAct;
+  /* Pointer to the range-and-bearing sensor */
+  CCI_RangeAndBearingSensor* m_pcRABSens;
+  // Robot Id number 0-N
+  int my_id;
 };
 
