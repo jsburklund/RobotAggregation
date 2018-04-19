6c1c3b1808b6af4455f809d81ea83bc7ca0e2dfa
Peter Mitrano <mitranopeter@gmail.com>
2018-04-19 03:21:40 -0400

    finish viz of grids and test on all of the first 100 parameters

--- bin/visualize_grid_search_output.py
+++ bin/visualize_grid_search_output.py
@@ -4,32 +4,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser("Visualize a set of output files from grid search")
     parser.add_argument("grid_search_outputs", nargs="+")
-    parser.add_argument("x_param", type=int)
-    parser.add_argument("y_param", type=int)
     parser.add_argument("--resolution", type=int, default=10)
 
     args = parser.parse_args()
 
-    plt.figure()
-    plt.xlabel("param {:d}".format(args.x_param))
-    plt.ylabel("param {:d}".format(args.y_param))
-    plt.title("N-Class Segregation Cost")
+    for x_param in range(6):
+        for y_param in range(x_param + 1, 6):
 
-    shape = (args.resolution, args.resolution)
-    cost_image = np.ones(shape) * 1e24
-    for grid_search_output_filename in args.grid_search_outputs:
-        data = np.genfromtxt(grid_search_output_filename, delimiter=',')
-        for parameter_evaluation in data:
-            parameter_idx = int(parameter_evaluation[0])
-            cost = parameter_evaluation[-1]
-            row, col = np.unravel_index(parameter_idx, shape)
-            if cost < cost_image[row, col]:
-                cost_image[row, col] = parameter_evaluation[-1]
+            plt.figure()
+            plt.xlabel("param {:d}".format(x_param))
+            plt.ylabel("param {:d}".format(y_param))
+            plt.title("N-Class Segregation Cost")
 
-    plt.imshow(cost_image)
+            shape = tuple([args.resolution]*6)
+            cost_image = np.ones((args.resolution, args.resolution)) * 1e24
+            for grid_search_output_filename in args.grid_search_outputs:
+                data = np.genfromtxt(grid_search_output_filename, delimiter=',')
+                for parameter_evaluation in data:
+                    parameter_idx = int(parameter_evaluation[0])
+                    cost = parameter_evaluation[-1]
+                    indeces = np.unravel_index(parameter_idx, shape)
+                    row = indeces[x_param]
+                    col = indeces[y_param]
+                    if cost < cost_image[row, col]:
+                        cost_image[row, col] = parameter_evaluation[-1]
+
+            plt.imshow(1 / cost_image)
 
     plt.show()
 
