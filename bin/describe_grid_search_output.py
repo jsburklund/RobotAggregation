c706b20f68b0f1758e44a0cd3dc636939f43db76
Peter Mitrano <mitranopeter@gmail.com>
2018-04-26 14:27:43 -0400

    more params
     - add more checks in describe_grid_serach_output

--- bin/describe_grid_search_output.py
+++ bin/describe_grid_search_output.py
@@ -23,11 +23,31 @@ def main():
                 print("expected param idx {:d}, got {:d}". format(i, j))
                 return
             i += 1
-        info[grid_search_output_filename] = (first_param_idx, last_param_idx)
+        param_range = (first_param_idx, last_param_idx)
+        info[grid_search_output_filename] = param_range
 
     info = sorted(info.items(), key=lambda item: item[1][0])
-    for file, idxs in info:
-        print("file [{:s}] has parameters {:d}-{:d}".format(file, *idxs))
+
+    # check that we have all the parameters and didn't skip any
+    def check_continuous():
+        range_min = info[0][1][0]
+        last_param_range = None
+        for _, param_range in info:
+            if last_param_range:
+                if last_param_range[1] + 1 != param_range[0]:
+                    print("missing parameters {:d}-{:d}".format(param_range[0], last_param_range[1]+1))
+                    return False
+            last_param_range = param_range
+            range_max = param_range[1]
+        return range_min, range_max
+
+    continuous_param_range = check_continuous()
+    if continuous_param_range:
+        print("all parameters from {:d} to {:d} are present".format(*continuous_param_range))
+
+
+    for file, param_range in info:
+        print("file [{:s}] has parameters {:d}-{:d}".format(file, *param_range))
 
 
 if __name__ == "__main__":
