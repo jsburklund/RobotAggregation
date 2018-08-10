e3aa0b380e721b43a8c0dac86f46e41ea566ef49
Peter Mitrano <mitranopeter@gmail.com>
2018-08-10 00:34:33 -0400

    rerun evolv on cluster

--- cluster_jobs/evolve.sh
+++ cluster_jobs/evolve.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -N 1
-#SBATCH -n 36
-#SBATCH -t 12:00:00
+#SBATCH -n 38
+#SBATCH -t 4:00:00
 #SBATCH --mem 64GB
 #SBATCH -J evolve
 
@@ -26,6 +26,7 @@ rm -rf $WORKDIR && mkdir -p $WORKDIR && cd $WORKDIR
 
 # setup the directories and files we expect
 cp -r $PROJDIR/bin .
+cp -r $PROJDIR/venv .
 cp -r $PROJDIR/build .
 cp -r $PROJDIR/controllers .
 cp -r $PROJDIR/loop_functions .
@@ -36,7 +37,8 @@ cp $PROJDIR/python/cma_evolve.py .
 
 # Execute program (this also writes files in work dir)
 echo $WORKDIR
-./cma_evolve.py -p 36 -t 1 experiments/1_class/*.argos experiments/2_class/*.argos experiments/4_class/*.argos build/loop_functions/libcluster_metric.so -g 10
+source venv/bin/activate
+./cma_evolve.py -p 38 -t 1 experiments/1_class/*.argos experiments/2_class/*.argos experiments/4_class/*.argos build/loop_functions/libsegregation_loop_function.so -g 100
 
 # Transfer generated files into home directory
 cp cma_evolve_output_*.txt $DATADIR
