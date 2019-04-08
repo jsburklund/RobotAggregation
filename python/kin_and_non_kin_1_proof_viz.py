1457ccbb3fedf90d93b3fa617ef1c2e871c67fb8
Peter Mitrano <mitranopeter@gmail.com>
2019-04-08 12:20:11 -0400

    testing shows that non-kin aggregate in most of the best 100 params

--- python/kin_and_non_kin_1_proof_viz.py
+++ python/kin_and_non_kin_1_proof_viz.py
@@ -19,6 +19,7 @@ def main():
     dt = 0.1
     V = 0.2
     theta_kin = 2 * V / (3 * l) * dt
+    theta_non_kin = 2 * V / l * dt
 
     xs = np.arange(sqrt(3) * r, 0.88825, 0.001)
     ys = np.arange(-r, r, 0.001)
@@ -26,6 +27,7 @@ def main():
     W = len(xs)
     H = len(ys)
 
+    # KIN
     img = np.ones((W, H)) * 1e9
     for xi, x in enumerate(xs):
         for yi, y in enumerate(ys):
@@ -40,11 +42,36 @@ def main():
                 d = np.linalg.norm(cj - ci)
                 d_prime = np.linalg.norm(cj_prime - ci_prime)
                 delta = d - d_prime
+                if delta < 0:
+                    print("proof does not hold at : {}, {}, {} : ".format(x, y, alpha, delta))
                 if delta < img[xi, H - 1 - yi]:
                     img[xi, H - 1 - yi] = delta
 
     plt.figure()
-    plt.imshow(img.T)
+    plt.title("kin")
+    plt.imshow(img.T, extent=[xs[0], xs[-1], ys[0], ys[-1]])
+
+    # NON KIN
+    img = np.ones((W, H)) * 1e9
+    for xi, x in enumerate(xs):
+        for yi, y in enumerate(ys):
+            for alpha in alphas:
+                pj = np.array([x, y])
+                ci = np.array([0, -l / 4])
+                cj = pj + rot(alpha - np.pi / 2) @ np.array([l / 4, 0])
+                ci_prime = rot(theta_non_kin) @ ci
+                cj_prime = rot(theta_non_kin) @ (cj - pj) + pj
+                d = np.linalg.norm(cj - ci)
+                d_prime = np.linalg.norm(cj_prime - ci_prime)
+                delta = d - d_prime
+                if delta < 0:
+                    print("proof does not hold at : {}, {}, {} : ".format(x, y, alpha, delta))
+                if delta < img[xi, H - 1 - yi]:
+                    img[xi, H - 1 - yi] = delta
+
+    plt.figure()
+    plt.title("non-kin")
+    plt.imshow(img.T, extent=[xs[0], xs[-1], ys[0], ys[-1]])
     plt.show()
 
 
